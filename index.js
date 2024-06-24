49dd1e3300f357adef899053a53dc08ac7e0c9e9
Samueltz24 <108952038+Samueltz24@users.noreply.github.com>
2024-06-23 21:15:24 -0300

    Pricipal

---
+++ index.js
@@ -0,0 +1,19 @@
+new Glider(document.querySelector('.glider'), {
+  slidesToShow: 1,
+  dots: '#dots',
+  draggable: true,
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
+
+new Glider(document.querySelector('.glide'), {
+  slidesToShow: 1,
+  dots: '#dots',
+  draggable: true,
+  arrows: {
+    prev: '.glider-prev',
+    next: '.glider-next'
+  }
+});
